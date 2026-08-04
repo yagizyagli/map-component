c3659ebea2c56fc8aade41aefa68bf114abbdf24
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-05 01:07:15 +0300

    Create index.js

---
+++ chart/index.js
@@ -0,0 +1,19 @@
+export { default as CustomChart } from "./CustomChart.js";
+
+export { default as DEFAULTS } from "./ChartDefaults.js";
+
+export * from "./colors.js";
+
+export { default as DatasetManager } from "./DatasetManager.js";
+
+export { default as TooltipManager } from "./TooltipManager.js";
+
+export { default as LegendManager } from "./LegendManager.js";
+
+export { default as ThemeManager } from "./ThemeManager.js";
+
+export { default as ExportManager } from "./ExportManager.js";
+
+export { default as AnimationManager } from "./AnimationManager.js";
+
+export { default as PluginManager } from "./PluginManager.js";
