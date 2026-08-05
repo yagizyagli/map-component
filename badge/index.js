cb758b7b087f1948a4600bf93ecbe7c756d122e0
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-06 00:01:10 +0300

    Add badge exports for Badge, BADGE_DEFAULTS, and ThemeManager

---
+++ badge/index.js
@@ -0,0 +1,12 @@
+/* ===========================================================
+ *
+ * BADGE EXPORTS
+ *
+ * ===========================================================
+ */
+
+export { default as Badge } from "./badge.js";
+
+export { default as BADGE_DEFAULTS } from "./BadgeDefaults.js";
+
+export { default as ThemeManager } from "./ThemeManager.js";
