eb0d7ea13ae9a3430acedcaade479660d8823925
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-05 15:18:04 +0300

    Create index.js

---
+++ table/index.js
@@ -0,0 +1,19 @@
+export { default as Table } from "./table.js";
+
+export { default as DataManager } from "./DataManager.js";
+
+export { default as ColumnManager } from "./ColumnManager.js";
+
+export { default as SortManager } from "./SortManager.js";
+
+export { default as FilterManager } from "./FilterManager.js";
+
+export { default as PaginationManager } from "./PaginationManager.js";
+
+export { default as SelectionManager } from "./SelectionManager.js";
+
+export { default as ExportManager } from "./ExportManager.js";
+
+export { default as ThemeManager } from "./ThemeManager.js";
+
+export { default as VirtualScrollManager } from "./VirtualScrollManager.js";
