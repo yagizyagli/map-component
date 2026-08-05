d0dffead2bdbd7d7519505cde729d1df4d22e8fa
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-05 19:54:27 +0300

    Create FormState.js

---
+++ form/FormState.js
@@ -0,0 +1,164 @@
+/* ===========================================================
+ *
+ * FORM STATE
+ *
+ * Production Grade Form State Manager
+ *
+ * ===========================================================
+ */
+
+export default class FormState {
+
+    constructor(component = null) {
+
+        this.component = component;
+
+        this.reset();
+
+    }
+
+    bind(component) {
+
+        this.component = component;
+
+    }
+
+    set(key, value) {
+
+        this.state[key] = value;
+
+        this.commit();
+
+    }
+
+    get(key) {
+
+        return this.state[key];
+
+    }
+
+    update(values = {}) {
+
+        Object.assign(
+            this.state,
+            values
+        );
+
+        this.commit();
+
+    }
+
+    markDirty() {
+
+        this.state.dirty = true;
+
+        this.commit();
+
+    }
+
+    markTouched() {
+
+        this.state.touched = true;
+
+        this.commit();
+
+    }
+
+    markSubmitted() {
+
+        this.state.submitted = true;
+
+        this.commit();
+
+    }
+
+    setValid(valid = true) {
+
+        this.state.valid = Boolean(valid);
+
+        this.commit();
+
+    }
+
+    setLoading(loading = true) {
+
+        this.state.loading = Boolean(loading);
+
+        this.commit();
+
+    }
+
+    isDirty() {
+
+        return this.state.dirty;
+
+    }
+
+    isTouched() {
+
+        return this.state.touched;
+
+    }
+
+    isSubmitted() {
+
+        return this.state.submitted;
+
+    }
+
+    isValid() {
+
+        return this.state.valid;
+
+    }
+
+    isLoading() {
+
+        return this.state.loading;
+
+    }
+
+    reset() {
+
+        this.state = {
+
+            dirty: false,
+
+            touched: false,
+
+            submitted: false,
+
+            valid: true,
+
+            loading: false
+
+        };
+
+    }
+
+    commit() {
+
+        if (
+            !this.component
+        )
+            return;
+
+        if (
+            typeof this.component.render === "function"
+        ) {
+
+            this.component.render();
+
+        }
+
+    }
+
+    destroy() {
+
+        this.reset();
+
+        this.component = null;
+
+    }
+
+}
