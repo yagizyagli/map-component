ac819fa48922dfa58e7b1515117142e27c7a81ae
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-05 21:53:44 +0300

    Create index.js

---
+++ tooltip/index.js
@@ -0,0 +1,16 @@
+/* ===========================================================
+ *
+ * TOOLTIP EXPORTS
+ *
+ * ===========================================================
+ */
+
+export { default as Tooltip } from "./tooltip.js";
+
+export { default as TOOLTIP_DEFAULTS } from "./TooltipDefaults.js";
+
+export { default as PositionManager } from "./PositionManager.js";
+
+export { default as ThemeManager } from "./ThemeManager.js";
+
+export { default as AnimationManager } from "./AnimationManager.js";
