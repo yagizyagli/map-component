f23190e069e86af0710382b5cffb84399dab43f6
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-05 23:57:33 +0300

    Create StateManager.js

---
+++ card/StateManager.js
@@ -0,0 +1,121 @@
+/* ===========================================================
+ *
+ * STATE MANAGER
+ *
+ * Production Grade Card State Manager
+ *
+ * ===========================================================
+ */
+
+export default class StateManager {
+
+    constructor(component = null) {
+
+        this.component = component;
+
+        this.state = {
+
+            loading: false,
+
+            selected: false,
+
+            expanded: false,
+
+            disabled: false
+
+        };
+
+    }
+
+    bind(component) {
+
+        this.component = component;
+
+    }
+
+    set(key, value) {
+
+        if (!(key in this.state))
+            return false;
+
+        this.state[key] = Boolean(value);
+
+        this.commit();
+
+        return true;
+
+    }
+
+    get(key) {
+
+        return this.state[key];
+
+    }
+
+    toggle(key) {
+
+        if (!(key in this.state))
+            return false;
+
+        this.state[key] = !this.state[key];
+
+        this.commit();
+
+        return true;
+
+    }
+
+    update(values = {}) {
+
+        for (const [key, value] of Object.entries(values)) {
+
+            if (key in this.state) {
+
+                this.state[key] = Boolean(value);
+
+            }
+
+        }
+
+        this.commit();
+
+    }
+
+    reset() {
+
+        this.state = {
+
+            loading: false,
+
+            selected: false,
+
+            expanded: false,
+
+            disabled: false
+
+        };
+
+        this.commit();
+
+    }
+
+    commit() {
+
+        if (
+            this.component &&
+            typeof this.component.render === "function"
+        ) {
+
+            this.component.render();
+
+        }
+
+    }
+
+    destroy() {
+
+        this.component = null;
+
+    }
+
+}
