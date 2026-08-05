7be8ce87911cf2d6af9860e2ddc1bc5912e555d3
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-06 00:06:52 +0300

    Add toast exports for various managers and defaults

---
+++ toast/index.js
@@ -0,0 +1,16 @@
+/* ===========================================================
+ *
+ * TOAST EXPORTS
+ *
+ * ===========================================================
+ */
+
+export { default as Toast } from "./toast.js";
+
+export { default as TOAST_DEFAULTS } from "./ToastDefaults.js";
+
+export { default as ToastManager } from "./ToastManager.js";
+
+export { default as ThemeManager } from "./ThemeManager.js";
+
+export { default as AnimationManager } from "./AnimationManager.js";
