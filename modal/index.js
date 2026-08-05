73f9952e912c42ac32dc4e7c9cb51c1ef8d39f6f
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-05 20:59:18 +0300

    Create index.js

---
+++ modal/index.js
@@ -0,0 +1,16 @@
+/* ===========================================================
+ *
+ * MODAL EXPORTS
+ *
+ * ===========================================================
+ */
+
+export { default as Modal } from "./modal.js";
+
+export { default as MODAL_DEFAULTS } from "./ModalDefaults.js";
+
+export { default as StateManager } from "./StateManager.js";
+
+export { default as ThemeManager } from "./ThemeManager.js";
+
+export { default as FocusManager } from "./FocusManager.js";
