174226e45be039ced3d3b94a4ef2453f1fb2a250
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-07 19:58:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-export * from "./button";
-export * from "./badge";
-export * from "./calendar";
-export * from "./chart";
-export * from "./form";
+import "./badge/badge.js";
+import "./button/button.js";
+import  "./card/card.js";
+import "./chart/chart.js";
+import "./Calendar/calendar.js";
+import "./drawer/drawer.js";
+import "./dropdown/dropdown.js";
+import "./form/form.js";
 import "./map/map.js";
+import "./modal/modal.js";
+import "./spinner/spinner.js";
+import "./table/table.js";
+import "./toast/toast.js";
+import "./tooltip/tooltip.js";
+
+
+
+export * from "./button/button.js";
+export * from "./badge/badge.js";
+export * from "./card/card.js";
+export * from "./Calendar/calendar.js";
+export * from "./chart/chart.js";
+export * from "./drawer/drawer.js";
+export * from "./dropdown/dropdown.js";
+export * from "./form/form.js";
 export * from "./map/map.js";
-export * from "./modal";
-export * from "./table";
-export * from "./toast";
-export * from "./tooltip";
+export * from "./modal/modal.js";
+export * from "./spinner/spinner.js";
+export * from "./table/table.js";
+export * from "./toast/toast.js";
+export * from "./tooltip/tooltip.js";
