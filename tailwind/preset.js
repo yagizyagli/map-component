595c669de3f206915f36d8cf307b1b8049d10d25
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-07 17:44:10 +0300

    Create preset.js

---
+++ tailwind/preset.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+    theme: {
+
+        extend: {
+
+            colors: {
+
+                ui: {
+
+                    primary: "var(--ui-primary)",
+                    secondary: "var(--ui-secondary)",
+                    success: "var(--ui-success)",
+                    warning: "var(--ui-warning)",
+                    danger: "var(--ui-danger)",
+
+                    background: "var(--ui-background)",
+                    surface: "var(--ui-surface)",
+
+                    text: "var(--ui-text)",
+                    muted: "var(--ui-muted)",
+
+                    border: "var(--ui-border)"
+
+                }
+
+            },
+
+            borderRadius: {
+
+                ui: "var(--ui-radius)"
+
+            },
+
+            boxShadow: {
+
+                ui: "var(--ui-shadow)"
+
+            }
+
+        }
+
+    }
+
+};
