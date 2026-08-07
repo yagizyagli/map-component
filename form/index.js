11c565456c328c24b5677daa8328597c5f7eb89d
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-07 16:29:40 +0300

    Update index.js

--- form/index.js
+++ form/index.js
@@ -2,11 +2,6 @@ export { default as Form } from "./Form.js";
 
 export { default as FORM_DEFAULTS } from "./FormDefaults.js";
 
-export { default as ValidationManager } from "./ValidationManager.js";
-export { default as FieldRegistry } from "./FieldRegistry.js";
-export { default as FormState } from "./FormState.js";
-export { default as ThemeManager } from "./ThemeManager.js";
-
 export { default as Input } from "./fields/input.js";
 export { default as Select } from "./fields/select.js";
 export { default as Checkbox } from "./fields/checkbox.js";
