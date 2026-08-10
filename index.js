2aa9b3aba59fb4c736a89a14788d80b863507b2f
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-10 16:10:11 +0300

    Add Chart component export to index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ export { default as Badge } from "./badge/badge.js";
 export { default as Button } from "./button/button.js";
 export { default as Calendar } from "./Calendar/calendar.js";
 export { default as Card } from "./card/card.js";
+export { default as Chart } from "./chart/chart.js";
 export { default as Drawer } from "./drawer/drawer.js";
 export { default as Dropdown } from "./dropdown/dropdown.js";
 export { default as Form } from "./form/form.js";
