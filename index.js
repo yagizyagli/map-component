ab7b30195c0cb1d4892451a94f5e24bcbe4a23d3
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-08-10 16:09:22 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Web Components Library
+ * Main Entry Point
+ *
+ * Author: yagizyagli
+ */
+
+export { default as Badge } from "./badge/badge.js";
+export { default as Button } from "./button/button.js";
+export { default as Calendar } from "./Calendar/calendar.js";
+export { default as Card } from "./card/card.js";
+export { default as Drawer } from "./drawer/drawer.js";
+export { default as Dropdown } from "./dropdown/dropdown.js";
+export { default as Form } from "./form/form.js";
+export { default as Map } from "./map/map.js";
+export { default as Modal } from "./modal/modal.js";
+export { default as Spinner } from "./spinner/spinner.js";
+export { default as Table } from "./table/table.js";
+export { default as Toast } from "./toast/toast.js";
+export { default as Tooltip } from "./tooltip/tooltip.js";
